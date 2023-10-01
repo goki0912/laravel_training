3b351c48a73d6e630f91a978692b65edeb0015df
Goki Kanno <goki.kanno@gmail/com>
2023-10-01 21:26:51 +0900

    一旦

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,21 +8,43 @@
   <title>Document</title>
   @vite('resources/css/app.css')
 </head>
+
 <body>
   <form action="">
-    <h2>システムへのご意見をお聞かせください</h2>
-    <label for="">氏名</label>
-    <input type="text" required>
-    <label for="">性別</label>
-    <input type="radio" name="" id="man" value="man" checked><label for="man">男性</label>
-    <input type="radio" name="" id="man" value="woman"> <label for="">女性</label> 
-    <label for="">メールアドレス</label>
-    <input type="email" name="" id="">
-    <label for="">メール送信可否</label>
-    <input type="checkbox" name="" id="">
-    <label for="">ご意見</label>
-    <textarea name="" id="" cols="30" rows="10"></textarea>
-    <input type="submit" value="確認" class="text-white py-2 px-4 rounded">
+    <h1 class="text-xl">システムへのご意見をお聞かせください</h1>
+    <div class="">
+      <label for="">氏名</label>
+      <input type="text" placeholder="入力してください" required>
+    </div>
+    <div class="">
+      <label for="">性別</label>
+      <input type="radio" name="" id="man" value="man" checked><label for="man">男性</label>
+      <input type="radio" name="" id="man" value="woman"> <label for="">女性</label>
+    </div>
+    <div>
+      <label for="">年代</label>
+      <select name="" id="">
+        
+      </select>
+    </div>
+    <div class="">
+      </div> <label for="">メールアドレス</label>
+      <input type="email" name="" id="" placeholder="入力してください" required>
+      </div>
+    </div>
+    <div class="">
+      <label for="">メール送信可否</label>
+    </div>
+    <div>
+      <p>登録したメールアドレスにメールマガジンをお送りしてもよろしいですか？</p>
+      <input type="checkbox" name="" id="">送信を許可します
+    </div>
+    <div class="">
+      <label for="">ご意見</label>
+      <textarea class="resize-none" name="" id="" cols="30" rows="10" placeholder="入力してください"></textarea>
+    </div>
+    <input type="submit" value="確認" class="text-white bg-red-500 py-2 px-4 rounded">
   </form>
 </body>
+
 </html>

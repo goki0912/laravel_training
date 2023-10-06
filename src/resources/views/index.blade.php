5cc97964b512f33340dd568409739a07f4be82f9
Goki Kanno <goki.kanno@gmail/com>
2023-10-06 22:46:08 +0900

    １０K

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,11 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
 @include('components.head')
 
 
 <body>
   <form action="{{route('confirm')}}" method="POST">
     @csrf
-    <h1 class="text-xl">システムへのご意見をお聞かせください</h1>
-    <div class="">
+    <div class="text-center text-xl font-bold mb-7">システムへのご意見をお聞かせください</div>
+    <div class="text-center">
       <label for="fullname">氏名</label>
       <input type="text" name="fullname" id="fullname" placeholder="入力してください">
       @error('fullname')
@@ -15,7 +17,7 @@
       </div>
       @enderror
     </div>
-    <div class="">
+    <div class="text-center">
       <label for="gender">性別</label>
       <label for="man">
         <input type="radio" name="gender" id="man" value="1" checked>男性
@@ -30,7 +32,7 @@
       </div>
       @enderror
     </div>
-    <div>
+    <div class="text-center">
       <label for="age">年代</label>
       <select name="age" id="age">
         <option value="">選択してください</option>
@@ -45,27 +47,29 @@
       </div>
       @enderror
     </div>
-    <div class="mail">
-    </div> <label for="mail">メールアドレス</label>
-    <input type="email" name="mail" id="mail" placeholder="入力してください" >
-    </div>
-    @error('mail')
+    <div class="text-center">
+      <label for="mail">メールアドレス</label>
+      <input type="email" name="mail" id="mail" placeholder="入力してください">
+
+      @error('mail')
       <!-- mailフィールドのエラーメッセージを表示 -->
-      <div class=""><p class="bg-red-500 text-white">{{ $message }}</p></div>
+      <div class="">
+        <p class="bg-red-500 text-white">{{ $message }}</p>
+      </div>
       @enderror
     </div>
-    <div class="">
+    <div class="text-center">
       <label for="">メール送信可否</label>
     </div>
-    <div>
+    <div class="text-center">
       <p>登録したメールアドレスにメールマガジンをお送りしてもよろしいですか？</p>
       <input type="checkbox" value='permit' name="permit" id="permit">送信を許可します
     </div>
-    <div class="">
+    <div class="text-center">
       <label for="opinion">ご意見</label>
       <textarea class="resize-none" name="opinion" id="opinion" cols="30" rows="10" placeholder="入力してください"></textarea>
     </div>
-    <input type="submit" value="確認" class="text-white bg-red-500 py-2 px-4 rounded">
+    <input type="submit" value="確認" class="text-white bg-red-500 py-2 px-4 rounded text-center">
   </form>
 </body>
 

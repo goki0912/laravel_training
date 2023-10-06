8c42246c332bf47cc0a54d1c6f02e0c15797b80d
Goki Kanno <goki.kanno@gmail/com>
2023-10-06 23:41:46 +0900

    tailwind解決

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -3,12 +3,12 @@
 @include('components.head')
 
 
-<body>
-  <form action="{{route('confirm')}}" method="POST">
+<body class="">
+  <form action="{{route('confirm')}}" method="POST" class="mx-20 bg-gray-500">
     @csrf
-    <div class="text-center text-xl font-bold mb-7">システムへのご意見をお聞かせください</div>
-    <div class="text-center">
-      <label for="fullname">氏名</label>
+    <div class="text-center text-xl font-bold mb-6">システムへのご意見をお聞かせください</div>
+    <div class="flex">
+      <label for="fullname" class="w-20 ">氏名</label>
       <input type="text" name="fullname" id="fullname" placeholder="入力してください">
       @error('fullname')
       <!-- fullnameフィールドのエラーメッセージを表示 -->
@@ -17,7 +17,7 @@
       </div>
       @enderror
     </div>
-    <div class="text-center">
+    <div class="">
       <label for="gender">性別</label>
       <label for="man">
         <input type="radio" name="gender" id="man" value="1" checked>男性
@@ -32,7 +32,7 @@
       </div>
       @enderror
     </div>
-    <div class="text-center">
+    <div class="">
       <label for="age">年代</label>
       <select name="age" id="age">
         <option value="">選択してください</option>
@@ -47,7 +47,7 @@
       </div>
       @enderror
     </div>
-    <div class="text-center">
+    <div class="">
       <label for="mail">メールアドレス</label>
       <input type="email" name="mail" id="mail" placeholder="入力してください">
 
@@ -58,19 +58,19 @@
       </div>
       @enderror
     </div>
-    <div class="text-center">
+    <div class="">
       <label for="">メール送信可否</label>
     </div>
-    <div class="text-center">
+    <div class="">
       <p>登録したメールアドレスにメールマガジンをお送りしてもよろしいですか？</p>
       <input type="checkbox" value='permit' name="permit" id="permit">送信を許可します
     </div>
-    <div class="text-center">
+    <div class="">
       <label for="opinion">ご意見</label>
       <textarea class="resize-none" name="opinion" id="opinion" cols="30" rows="10" placeholder="入力してください"></textarea>
     </div>
-    <input type="submit" value="確認" class="text-white bg-red-500 py-2 px-4 rounded text-center">
+    <input type="submit" value="確認" class="text-white bg-blue-500 py-2 px-4 rounded text-center">
   </form>
 </body>
-
+<script src="https://cdn.tailwindcss.com"></script>
 </html>

7c62af082b4ce919adbe8733460889b4616458f1
Goki Kanno <goki.kanno@gmail/com>
2023-10-17 14:09:40 +0900

    検索モーダルスタート

--- src/app/Http/Controllers/AnswerController.php
+++ src/app/Http/Controllers/AnswerController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Answer;
+use App\Models\Age;
 
 class AnswerController extends Controller
 {
     public function index()
     {
-        $answers=Answer::paginate(10);
-        return view('auth.index',compact('answers'));
+        $answers = Answer::paginate(10);
 
+        view()->composer('auth.index', function ($view) {
+            $ages = Age::all();
+            $view->with('ages', $ages);
+        });
+
+        return view('auth.index', compact('answers'));
     }
 }

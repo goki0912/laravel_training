e82e4cb69f45b3215ba7645499b2d5deb86e7442
goki0912 <goki.kanno@gmail.com>
2023-10-25 02:07:04 +0900

    検索機能の日付とキーワードできた

--- src/app/Http/Controllers/AnswerController.php
+++ src/app/Http/Controllers/AnswerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Answer;
 use App\Models\Age;
+use Carbon\Carbon;
 
 class AnswerController extends Controller
 {
@@ -17,33 +18,46 @@ class AnswerController extends Controller
     //     return view('auth.index', compact('answers'));
     // }
 
-        public function index(Request $request)
-        {
-            $query=Answer::query();
+    public function index(Request $request)
+    {
+        $query = Answer::query();
 
-            if ($request->filled('name')) {
-                $query->where('fullname','like','%'.$request->input('name').'%');
-            }
+        if ($request->filled('name')) {
+            $query->where('fullname', 'like', '%' . $request->input('name') . '%');
+        }
 
-            if ($request->filled('age')) {
-                $query->where('age_id',$request->input('age'));
-            }
+        if ($request->filled('age')) {
+            $query->where('age_id', $request->input('age'));
+        }
 
-            if ($request->filled('gender')) {
-                $query->where('gender',$request->input('gender'));
-            }
+        if ($request->filled('gender')) {
+            $query->where('gender', $request->input('gender'));
+        }
 
-            if ($request->filled('opinion')) {
-                $query->where('feedback','like','%'.$request.'%');
-            }
+        if ($request->filled('opinion')) {
+            $query->where('feedback', 'like', '%' . $request . '%');
+        }
 
-            $answers=$query->paginate(15);
+        if ($request->filled('start') && $request->filled('end')) {
+            $StartDateString=$request->input('start');
+            $EndDateString=$request->input('end');
+            $StartConvertedDate=Carbon::createFromFormat('m/d/Y',$StartDateString)->format('Y-m-d');
+            $EndConvertedDate=Carbon::createFromFormat('m/d/Y',$EndDateString)->format('Y-m-d');
+            $query->whereBetween('created_at',[$StartConvertedDate,$EndConvertedDate]);
+        }
+
+        if ($request->filled('keyword')) {
+            $query->where('mail', 'like', '%' . $request->input('keyword') . '%')
+                ->orWhere('feedback', 'like', '%' . $request->input('keyword') . '%');
+        }
+
+        $answers = $query->paginate(15);
 
         view()->composer('auth.index', function ($view) {
             $ages = Age::all();
             $view->with('ages', $ages);
         });
 
-            return view('auth.index',compact('answers'));
-        }
+        return view('auth.index', compact('answers'));
+    }
 }
